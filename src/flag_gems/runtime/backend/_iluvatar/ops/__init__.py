0c2694e0be9ce99794923c47bd846b4c4f7a06dd
taooo <gumptao2997@gmail.com>
2026-05-15 22:20:59 +0800

    Add reflection_pad2d iluvatar specialized operator implementation

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -1,6 +1,9 @@
 from .div import div_mode, div_mode_
+from .reflection_pad2d import reflection_pad2d, reflection_pad2d_out
 
 __all__ = [
     "div_mode",
     "div_mode_",
+    "reflection_pad2d",
+    "reflection_pad2d_out",
 ]
