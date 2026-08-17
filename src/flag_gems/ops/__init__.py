55378ffcd2c602baaaec8e0562376032fe5c7bc9
Dongxu-H <dxhan@baai.ac.cn>
2026-08-17 15:05:47 +0800

    Reorder import of _scaled_dot_product_efficient_attention
    Signed-off-by: Dongxu-H <dxhan@baai.ac.cn>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -74,11 +74,11 @@ from flag_gems.ops._prelu_kernel_backward import _prelu_kernel_backward
 from flag_gems.ops._reshape_alias import _reshape_alias
 from flag_gems.ops._resize_output import _resize_output
 from flag_gems.ops._safe_softmax import _safe_softmax
-from flag_gems.ops._scaled_dot_product_efficient_attention import (
-    _scaled_dot_product_efficient_attention,
-)
 from flag_gems.ops._scaled_dot_product_attention_math import (
     _scaled_dot_product_attention_math,
+)
+from flag_gems.ops._scaled_dot_product_efficient_attention import (
+    _scaled_dot_product_efficient_attention,
 )
 from flag_gems.ops._scaled_dot_product_flash_attention import (
     _scaled_dot_product_flash_attention,
